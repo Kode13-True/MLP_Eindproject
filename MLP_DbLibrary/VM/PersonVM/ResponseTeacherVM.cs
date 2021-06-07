6b97521dba14543323ebe6344f1518e0cbe5c41e
Kode13-True <deschepper6.ko@gmail.com>
2021-06-07 11:56:30 +0200

    Enhancement_Ko_2

--- MLP_DbLibrary/VM/PersonVM/ResponseTeacherVM.cs
+++ MLP_DbLibrary/VM/PersonVM/ResponseTeacherVM.cs
@@ -14,6 +14,6 @@ namespace MLP_DbLibrary.VM.PersonVM
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Description { get; set; }
-        public double Rating { get; set; }
+        public string Rating { get; set; }
     }
 }
